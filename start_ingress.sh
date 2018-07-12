05235c0c51893faf9e2487712ef276cd14220ccd
Mohit Shridhar <mohit.appi@gmail.com>
2018-07-12 17:19:47 +0800

    adding start script

--- start_ingress.sh
+++ start_ingress.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Setup ROS
-MASTER_URI=http://localhost:11311
-IP=localhost
+MASTER_URI=http://172.26.186.217:11311
+IP=172.26.186.217
 
 docker run --env ROS_MASTER_URI=$MASTER_URI --env ROS_IP=$IP --runtime=nvidia -it --rm --net host mohito/ingress:latest /bin/bash
