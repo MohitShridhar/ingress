9c8bbf22ae422d4f8f9d8b6e8edfc19d57535ba2
Mohit Shridhar <mohit.appi@gmail.com>
2018-08-27 10:59:33 +0800

    cleaning docker

--- start_ingress.sh
+++ start_ingress.sh
@@ -4,4 +4,4 @@
 MASTER_URI=http://127.0.0.1:11311
 IP=127.0.0.1
 
-docker run --env ROS_MASTER_URI=$MASTER_URI --env ROS_IP=$IP --runtime=nvidia -it --rm --net host mohito/ingress:latest /bin/bash
+docker run --env ROS_MASTER_URI=$MASTER_URI --env ROS_IP=$IP --runtime=nvidia -it --rm --net host mohito/ingress:v1.0 /bin/bash
