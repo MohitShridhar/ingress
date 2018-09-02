42773e65b2e83e627cade1e2dd7fef7ad7ebb023
Mohit Shridhar <mohit.appi@gmail.com>
2018-09-02 21:20:31 +0800

    updating README

--- start_ingress.sh
+++ start_ingress.sh
@@ -7,4 +7,4 @@ IP=127.0.0.1
 # CUDA Setup
 GPU_IDX=0
 
-docker run --env CUDA_VISIBLE=$GPU_IDX --env ROS_MASTER_URI=$MASTER_URI --env ROS_IP=$IP --runtime=nvidia -it --rm --net host mohito/ingress:v1.0 /bin/bash
+docker run --env CUDA_VISIBLE_DEVICES=$GPU_IDX --env ROS_MASTER_URI=$MASTER_URI --env ROS_IP=$IP --runtime=nvidia -it --rm --net host mohito/ingress:v1.0 /bin/bash
